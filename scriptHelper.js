196e91b943eec2103fc2c7ff2133d9c84ae33a7c
amy hufker <amyelizabethrosehufker@gmail.com>
2021-08-03 16:50:02 -0500

    fadas

--- scriptHelper.js
+++ scriptHelper.js
@@ -76,14 +76,14 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
         //  launchStatus.style.color = "rbg(199, 37, 78)";
         launchStatus.innerHTML="Shuttle NOT ready for launch yet";
         cargoStatus.innerHTML="Cargo mass too heavy for launch";
-        launchStatus.style.color ='red';
+        launchStatus.style.color ="red";
         fuelStatus.innerHTML="Fuel level too low for launch";
     }
     if(fuelLevel < 10000 && cargoLevel < 10000){
         list.style.visibility=  "visible";
         // launchStatus.style.color = "#C7254E";
         //  launchStatus.style.color = "rbg(199, 37, 78)";
-        launchStatus.style.color = 'red';
+        launchStatus.style.color = "red";
         launchStatus.innerHTML="Shuttle NOT ready for launch yet";
         fuelStatus.innerHTML="Fuel level too low for launch";
 
@@ -92,7 +92,7 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
         launchStatus.innerHTML = "Shuttle Ready for launch";
         // launchStatus.style.color = "#419f6a";
         // launchStatus.style.color = "rbg(65, 159, 106)";
-        launchStatus.style.color ='green';
+        launchStatus.style.color ="green";
         list.style.visibility = "hidden";
     }
 
