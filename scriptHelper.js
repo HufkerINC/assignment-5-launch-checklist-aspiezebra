64d700181d7f01bcfd0e604c787fe4603d69ece9
amy hufker <amyelizabethrosehufker@gmail.com>
2021-08-06 14:27:44 -0500

    faf

--- scriptHelper.js
+++ scriptHelper.js
@@ -4,7 +4,7 @@ require('isomorphic-fetch');
 
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
     // Here is the HTML formatting for our mission target div.
-/*
+
                  <h2>Mission Destination</h2>
                  <ol>
                      <li>Name: </li>
@@ -15,7 +15,7 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
                  </ol>
                  <img src="${imageUrl}">
                  `;
-*/
+
 
 }
 function validateInput(testInput) {
