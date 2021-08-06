7d3568cd9ac8af3e9ffd86e834d2bcdb1e5ff5a9
amy hufker <amyelizabethrosehufker@gmail.com>
2021-08-06 12:00:08 -0500

    afds

--- scriptHelper.js
+++ scriptHelper.js
@@ -44,36 +44,36 @@ function validateInput(testInput) {
 
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-    // let cargoInput = document.getElementById("cargoLevel");
-    // let fuelInput = document.getElementById("fuelLevel");
-    // let pilotInput = document.getElementById("pilotStatus");
-    // let copilotInput = document.getElementById("copilotStatus");
-    // let error1 = "";
-    // let error2 =""
-    // if (validateInput(pilot)==="Empty" || validateInput(copilot)=== "Empty"|| validateInput(fuelLevel)=== "Empty"|| validateInput(cargoLevel)=== "Empty"){
-    //     alert("All fields are required!");
+    let cargoInput = document.getElementById("cargoLevel");
+    let fuelInput = document.getElementById("fuelLevel");
+    let pilotInput = document.getElementById("pilotStatus");
+    let copilotInput = document.getElementById("copilotStatus");
+    let error1 = "";
+    let error2 =""
+    if (validateInput(pilot)==="Empty" || validateInput(copilot)=== "Empty"|| validateInput(fuelLevel)=== "Empty"|| validateInput(cargoLevel)=== "Empty"){
+        alert("All fields are required!");
 
-    // } else if (validateInput(pilot)==="Is a number"||validateInput(copilot)==="Is a number"||validateInput(cargoLevel) ==="Not a Number"||validateInput(fuelLevel) ==="Not a Number" ){
-    //     if(validateInput(pilot)==="Is a number"||validateInput(copilot)==="Is a number"){
-    //         if(validateInput(pilot)==="Is a number" &&validateInput(copilot)==="Is a number"  ){
-    //             error1 = "pilot and copilot"
-    //         } else if (validateInput(pilot)==="Is a number"){
-    //             error1 = "pilot"
-    //         } else if (validateInput(copilot)==="Is a number"){
-    //             error1 = "copilot"
-    //         }
-    //         console.log(`please enter a name for ${error1}`)
-    //     } else if (validateInput(cargoLevel) ==="Not a Number"||validateInput(fuelLevel) ==="Not a Number" ){
-    //         if(validateInput(cargoLevel) ==="Not a Number"&&validateInput(fuelLevel) ==="Not a Number" ){
-    //             error2 = "cargo level and fuel level"
-    //         } else if (validateInput(cargoLevel) ==="Not a Number"){
-    //             error2 = "cargo level"
-    //         } else if (validateInput(fuelLevel) ==="Not a Number" ){
-    //             error2 = "fuel level"
-    //         }
-    //         console.log(`please enter a number for ${error2}`)
+    } else if (validateInput(pilot)==="Is a number"||validateInput(copilot)==="Is a number"||validateInput(cargoLevel) ==="Not a Number"||validateInput(fuelLevel) ==="Not a Number" ){
+        if(validateInput(pilot)==="Is a number"||validateInput(copilot)==="Is a number"){
+            if(validateInput(pilot)==="Is a number" &&validateInput(copilot)==="Is a number"  ){
+                error1 = "pilot and copilot"
+            } else if (validateInput(pilot)==="Is a number"){
+                error1 = "pilot"
+            } else if (validateInput(copilot)==="Is a number"){
+                error1 = "copilot"
+            }
+            console.log(`please enter a name for ${error1}`)
+        } else if (validateInput(cargoLevel) ==="Not a Number"||validateInput(fuelLevel) ==="Not a Number" ){
+            if(validateInput(cargoLevel) ==="Not a Number"&&validateInput(fuelLevel) ==="Not a Number" ){
+                error2 = "cargo level and fuel level"
+            } else if (validateInput(cargoLevel) ==="Not a Number"){
+                error2 = "cargo level"
+            } else if (validateInput(fuelLevel) ==="Not a Number" ){
+                error2 = "fuel level"
+            }
+            console.log(`please enter a number for ${error2}`)
 
-    //     }
+        }
 
 
     }
@@ -91,33 +91,7 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
     //       alert("Pilot must be made of letters and/or words");
     //   }
 //CHECK FUEL AND CARGO LEVELS
-let pilotStatus = document.getElementById("pilotStatus");
-let copilotStatus = document.getElementById("copilotStatus");
-let fuelStatus = document.getElementById("fuelStatus");
-let cargoStatus = document.getElementById("cargoStatus");
-let launchStatus = document.getElementById("launchStatus");
 
-if (fuelLevel < 10000 || cargoLevel > 10000) {
-list.style.visibility= "visible";
-launchStatus.style.color = "rgb(199, 37, 78)";
-launchStatus.innerHTML = "Shuttle Not Ready for Launch";
-pilotStatus.innerHTML = `Pilot ${pilot} is ready for launch`;
-copilotStatus.innerHTML = `Co-pilot ${copilot} is ready for launch`;
-if (fuelLevel < 10000 && cargoLevel > 10000) {
-fuelStatus.innerHTML = "Fuel level too low for launch";
-cargoStatus.innerHTML = "Cargo mass too heavy for launch";
-} else if (fuelLevel < 10000) {
-fuelStatus.innerHTML = "Fuel level too low for launch";
-} else if (cargoLevel > 10000){
-cargoStatus.innerHTML = "Cargo mass too heavy for launch";
-}
-} else {
-list.style.visibility = "visible";
-launchStatus.style.color = 'rgb(65, 159, 106)';
-launchStatus.innerHTML = "Shuttle is Ready for Launch";
-pilotStatus.innerHTML = `Pilot ${pilot} is ready for launch`;
-copilotStatus.innerHTML = `Co-pilot ${copilot} is ready for launch`;
-}
 
 
 async function myFetch() {
